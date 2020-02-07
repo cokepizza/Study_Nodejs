ca3138c8907986dd3fb43de5c0c988ffecbab0f8
cokepizza <cokepizza3@gmail.com>
2020-02-07 19:35:01 +0900

    [backend] mongodb cluster config 업데이트

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://cokepizza:2FGJ9olhz9ucKQcc@cluster0-shard-00-00-toukv.mongodb.net:27017,cluster0-shard-00-01-toukv.mongodb.net:27017,cluster0-shard-00-02-toukv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
